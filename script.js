d8b173386c60273bc62c3c034e9ffb3f1bc351a0
ambitnanazwa <yoszhi@gmail.com>
2025-07-21 18:04:46 +0200

    Zmieniono źródło cytatów na API Chucka Norrisa

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const newQuoteButton = document.getElementById('new-quote');
 
 async function fetchQuote() {
   try {
-    const response = await fetch('https://zenquotes.io/api/random');
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
     const data = await response.json();
-    quote.textContent = `"${data[0].q}" - ${data[0].a}`;
+    quote.textContent = `"${data.value}"`;
   } catch (error) {
     console.error('Error fetching quote:', error);
     quote.textContent = 'Nie udało się pobrać cytatu. Spróbuj ponownie.';
@@ -13,3 +13,4 @@ async function fetchQuote() {
 }
 
 newQuoteButton.addEventListener('click', fetchQuote);
+
